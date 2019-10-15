8b7c2f5a6459261657f57880f128ec986d687fd5
DappsUni <43065381+dappsunilabs@users.noreply.github.com>
2019-10-14 19:36:21 -0700

    Create ICODappsUni.sol

---
+++ contracts/ICODappsUni.sol
@@ -0,0 +1,161 @@
+pragma solidity ^0.4.18;
+
+// ----------------------------------------------------------------------------
+// Safe math, ensures there is no numerical overflow condition
+// ----------------------------------------------------------------------------
+contract SafeMath {
+    function safeAdd(uint a, uint b) internal pure returns (uint c) {
+        c = a + b;
+        require(c >= a);
+    }
+    function safeSub(uint a, uint b) internal pure returns (uint c) {
+        require(b <= a);
+        c = a - b;
+    }
+    function safeMul(uint a, uint b) internal pure returns (uint c) {
+        c = a * b;
+        require(a == 0 || c / a == b);
+    }
+    function safeDiv(uint a, uint b) internal pure returns (uint c) {
+        require(b > 0);
+        c = a / b;
+    }
+}
+
+// ----------------------------------------------------------------------------
+// ERC Token Standard #20 Interface
+// https://github.com/ethereum/EIPs/blob/master/EIPS/eip-20-token-standard.md
+// ----------------------------------------------------------------------------
+contract ERC20Interface {
+    function totalSupply() public constant returns (uint);
+    function balanceOf(address tokenOwner) public constant returns (uint balance);
+    function transfer(address to, uint tokens) public returns (bool success);
+    function transferFrom(address from, address to, uint tokens) public returns (bool success);
+
+    event Transfer(address indexed from, address indexed to, uint tokens);
+    event Approval(address indexed tokenOwner, address indexed spender, uint tokens);
+}
+
+// ----------------------------------------------------------------------------
+// Owned contract
+// ----------------------------------------------------------------------------
+contract Owned {
+    address public owner;
+    address public newOwner;
+
+    event OwnershipTransferred(address indexed _from, address indexed _to);
+
+    function Owned() public {
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner {
+        require(msg.sender == owner);
+        _;
+    }
+
+    function transferOwnership(address _newOwner) public onlyOwner {
+        newOwner = _newOwner;
+    }
+    function acceptOwnership() public {
+        require(msg.sender == newOwner);
+        OwnershipTransferred(owner, newOwner);
+        owner = newOwner;
+        newOwner = address(0);
+    }
+}
+
+// ----------------------------------------------------------------------------
+// ERC20 Token, with the addition of symbol, name and decimals and assisted
+// token transfers
+// ----------------------------------------------------------------------------
+contract MyICOToken is ERC20Interface, Owned, SafeMath {
+    string public symbol;
+    string public  name;
+    uint8 public decimals;
+    uint256 public _totalSupply;
+    uint256 public _ownerSupply;
+    uint256 public hardCap;
+    uint public startDate;
+    uint public bonusEnds;
+    uint public endDate;
+    
+
+    mapping(address => uint) balances;
+    mapping(address => mapping(address => uint)) allowed;
+
+    // ------------------------------------------------------------------------
+    // This function is called when someone sends Ether to purchase Tokens during the ICO
+    // ------------------------------------------------------------------------
+    function MyICOToken() public {
+        symbol = "X";
+        name = "X";
+        decimals = 18;
+        bonusEnds = now + 1 weeks;
+        endDate = now + 7 weeks;
+        
+        _ownerSupply = 500000 * 1000000000000000000;
+        _totalSupply= _ownerSupply;
+        //Limit Total Tokens to ownersupply + 50,000
+        hardCap= _ownerSupply + 500000 * 1000000000000000000;
+        balances[msg.sender]=_ownerSupply;
+    }
+
+    // ------------------------------------------------------------------------
+    // Total supply
+    // ------------------------------------------------------------------------
+    function totalSupply() public constant returns (uint) {
+        return _totalSupply  - balances[address(0)];
+    }
+
+    // ------------------------------------------------------------------------
+    // Get the token balance for account `tokenOwner`
+    // ------------------------------------------------------------------------
+    function balanceOf(address tokenOwner) public constant returns (uint balance) {
+        return balances[tokenOwner];
+    }
+
+    // ------------------------------------------------------------------------
+    // Transfer the balance from token owner's account to `to` account
+    // ------------------------------------------------------------------------
+    function transfer(address to, uint tokens) public returns (bool success) {
+        balances[msg.sender] = safeSub(balances[msg.sender], tokens);
+        balances[to] = safeAdd(balances[to], tokens);
+        Transfer(msg.sender, to, tokens);
+        return true;
+    }
+
+    // ------------------------------------------------------------------------
+    // Transfer `tokens` from the `from` account to the `to` account
+    // ------------------------------------------------------------------------
+    function transferFrom(address from, address to, uint tokens) public returns (bool success) {
+        balances[from] = safeSub(balances[from], tokens);
+        allowed[from][msg.sender] = safeSub(allowed[from][msg.sender], tokens);
+        balances[to] = safeAdd(balances[to], tokens);
+        Transfer(from, to, tokens);
+        return true;
+    }
+
+
+    // ------------------------------------------------------------------------
+    // 500  MyICOTokens per 1 ETH. Provide bonus tokens before the bonus end date
+    // ------------------------------------------------------------------------
+    function ()  public payable {
+        require(now >= startDate && now <= endDate && _totalSupply <= hardCap);
+        uint tokens;
+        if (now <= bonusEnds) {
+            tokens = msg.value * 500;
+        } else {
+            tokens = msg.value * 700;
+        }
+        balances[msg.sender] = safeAdd(balances[msg.sender], tokens);
+        _totalSupply = safeAdd(_totalSupply, tokens);
+        Transfer(address(0), msg.sender, tokens);
+        owner.transfer(msg.value);
+    }
+
+}
+
+// ----------------------------------------------------------------------------
+//Adapted from https://github.com/bitfwdcommunity/ICO-tutorial/blob/master/ico-contract.sol
+// ----------------------------------------------------------------------------
